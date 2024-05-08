9af751f67a742fa032b441e2c606d1a4037364f6
Anthony Guinaudeau <anguinau@student.42.fr>
2024-05-08 20:45:30 +0200

    Update getenv.c

--- reverse/utils/getenv.c
+++ reverse/utils/getenv.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int ac, char **av)
-{
-    if (ac != 2) {
+int main(int ac, char **av){
+    if (ac != 2){
         printf("Usage: %s <env_var>\n", av[0]);
         return 1;
     }
